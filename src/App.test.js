4258fdb033b0ff020e86eba5ac99d67b01204526
tom-are <ratcliffe.tom+private@gmail.com>
2020-02-09 10:31:20 +0000

    Restyle App and hook up Game component

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders initial state correctly', () => {
+  const { getByText, getByTestId } = render(<App />);
+
+  const initialGameResult = getByText(/let's go!/i);
+  const header = getByTestId('app-header');
+
+  expect(initialGameResult).toBeInTheDocument();
+  expect(header).toBeInTheDocument();
 });
